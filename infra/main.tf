a8cc4a3789894309a6bfb81a2ffd3d91c3b3c527
Willian Cordeiro <107742701+williancordeiro1998@users.noreply.github.com>
2025-09-08 14:24:50 -0300

    Add files via upload

---
+++ infra/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "DevOpsAppServer"
+  }
+}
